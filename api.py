7cff02af23bf9c4a542930e8a28edb23ff1b2dbb
Braden Michelson <63371610+Braden50@users.noreply.github.com>
2021-12-14 03:40:50 -0800

    test7

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from queue import Queue
 
 
 codes = Queue(maxsize=10)
-redirect = 'https://braden-discord-bot.herokuapp.com/'
+redirect_url = 'https://braden-discord-bot.herokuapp.com/'
 os.environ['FLASK_APP'] = 'api.py'
 
 app = Flask(__name__)
@@ -17,12 +17,12 @@ def redirect(): # this method gets all users
     if request.method == 'GET':
         print("GET")
         code = request.args.get('code')
-        codes.put(f'{redirect}?code={code}')
+        codes.put(f'{redirect_url}?code={code}')
         return f'<h1>GET:{code}</h1>'
     elif request.method == 'POST':
         print("POST")
         code = request.args.get('code')
-        codes.put(f'{redirect}?code={code}')
+        codes.put(f'{redirect_url}?code={code}')
         return f'<h1>POST:{code}</h1>'
 
 
@@ -30,12 +30,12 @@ def redirect(): # this method gets all users
 def get_token(): # this method gets all users
     if request.method == 'GET':
         print("GET")
-        code = codes.get()
-        return f'{redirect}?code={code}'
+        url = codes.get()
+        return url
     elif request.method == 'POST':
         print("POST")
-        code = codes.get()
-        return f'{redirect}?code={code}'
+        url = codes.get()
+        return url
 
 
 
