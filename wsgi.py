5e22fd50df73786892b9b2c92be3bf57841ba95c
Braden Michelson <63371610+Braden50@users.noreply.github.com>
2021-12-24 14:51:07 -0700

    test with just booting up bot

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,11 @@ from bot import client
 
 
 if __name__ == "__main__":
-    x = threading.Thread(target=app.run)
-    x.start()
-    y = threading.Thread(target=client.run, args=(DISCORD_TOKEN,))
-    y.start()
-    # client.run(DISCORD_TOKEN)
+    # x = threading.Thread(target=app.run)
+    # x.start()
+    # y = threading.Thread(target=client.run, args=(DISCORD_TOKEN,))
+    # y.start()
+    client.run(DISCORD_TOKEN)
     # app.run()
 
 
